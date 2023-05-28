7aca1f9f0e1ea1daf907d7ad9f301a7ef55eb382
RUzai <RUzairov@gmail.com>
2023-05-28 18:55:02 +0300

    kotlin fix

--- android/app/src/main/kotlin/team/choodoo/flutter_template/MainActivity.kt
+++ android/app/src/main/kotlin/team/choodoo/flutter_template/MainActivity.kt
@@ -1,14 +1,12 @@
 // TODO: Package
 package team.choodoo.flutter_template
 
-import androidx.annotation.NonNull
 import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         if (BuildConfig.DEBUG) {
             window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
