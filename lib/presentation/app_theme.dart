6b41227782f3a64351b78279ea35e2c9deebd408
RUzai <RUzairov@gmail.com>
2023-08-05 14:25:09 +0300

    fix theme

--- lib/presentation/app_theme.dart
+++ lib/presentation/app_theme.dart
@@ -9,8 +9,21 @@ class AppTheme {
   static final ThemeData _theme = ThemeData(
     primarySwatch: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
-    textTheme: _textTheme,
+    textTheme: const TextTheme(),
   );
+}
 
-  static const TextTheme _textTheme = TextTheme();
+MaterialColor _createMaterialColor(Color color) {
+  return MaterialColor(color.value, {
+    50: color.withOpacity(0.05),
+    100: color.withOpacity(0.1),
+    200: color.withOpacity(0.2),
+    300: color.withOpacity(0.3),
+    400: color.withOpacity(0.4),
+    500: color.withOpacity(0.5),
+    600: color.withOpacity(0.6),
+    700: color.withOpacity(0.7),
+    800: color.withOpacity(0.8),
+    900: color.withOpacity(0.9),
+  });
 }
